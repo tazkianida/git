fc5781afe9ffb331be26bf1a73cc82412fd51404
Tazkianida <Tazkianida@TazkiAnida-PC>
2017-03-12 19:29:43 +0700

    # WARNING: head commit changed in the meantime
    Second Commit

---
+++ PropensiTest/src/main/java/com/example/model/PendaftarModel.java
@@ -0,0 +1,30 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PendaftarModel
+{
+    private String username;
+    private String noId;
+    private String namaId;
+    private String namaIjazah;
+    private String noHp;
+    private String noTelp;
+    private String negara;
+    private String kewarganegaraan;
+    private String alamatTetap;
+    private String jenisId;
+    private String alamatSekarang;
+    private String provinsi;
+    private String kota;
+    private String jenisKelamin;
+    //foto belum
+    //tanggal lahir belum
+    
+    
+}
